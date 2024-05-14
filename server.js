665df5534768af61934b395da45414215aedf35e
Francisco-Fredes <francisco.a.fredes@gmail.com>
2024-05-13 20:57:34 -0400

    agregando css, funcionalidad jimp y almacenando imagen modificada

--- server.js
+++ server.js
@@ -6,12 +6,47 @@ const Jimp = require('jimp');
 const app = express();
 const PORT = 3000;
 
+//Definiendo assets como publico
 app.use(express.static(path.join(__dirname, 'assets')));
 
+//Middleware para revisar la url
+app.use(express.urlencoded({ extended: true }));
+
+//Vista default hacia el formulario con el html
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-  });       
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+//funcion post con jimp para realizar los cambios en la imagen
+app.post('/submit', async (req, res) => {    
+  // definiendo la url recibida 
+  const { url } = req.body;
+  //agregando un uuid de 6 caracteres para almacenarlos como el nobmre la imagen
+  const imagenId = uuidv4().slice(0, 6);
+  //funcion jimp
+  Jimp.read(url, (err, imagen) => {
+    if (err) {
+      console.error('Error al leer la imagen:', err);
+      return res.status(500).send('Error al procesar la imagen');
+    }
+    //cambios esn la imagen 
+    imagen.greyscale()
+          .resize(350, 350)
+          //directorio para guardar imagen
+          .write(path.join(__dirname, 'images', `${imagenId}.jpg`), (err) => {
+            if (err) {
+              console.error('Error al escribir la imagen:', err);
+              return res.status(500).send('Error al procesar la imagen');
+            }
+            console.log('Imagen procesada y guardada como:', `${imagenId}.jpg`);
+            res.send('Imagen procesada y guardada');
+          });
+  });
+  console.log('URL:', url);
+  console.log('Imagen ID:', imagenId);
+ 
+});
 
 app.listen(PORT, () => {
-    console.log(`Servidor andando en el puerto ${PORT}`);
+  console.log(`Servidor andando en el puerto ${PORT}`);
 });
